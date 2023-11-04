8f03344f8100c9ad1f51ba9e68c708de6e285104
Dhairya Kalia <84274491+kaliadhairya@users.noreply.github.com>
2023-11-04 21:05:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const accessKey="4_9f_ytaSpw00bY4qK4dfLRAywIzRLiZAcHwGDeM_Nk";
+
+const formEl=document.querySelector("form");
+const inputEl=document.getElementById("search-input");
+const searchRes=document.querySelector(".search-results");
+const showMore=document.getElementById("show-more-button");
+
+let inputData=""
+let page=1;
+
+async function searchImages(){
+    inputData=inputEl.value;
+    
+    const url= `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+    const response=await fetch(url);
+    const data=await response.json();
+
+    const results=data.results
+    if(page===1){
+        searchRes.innerHTML="";
+    }
+    results.map((result)=>{
+        const imageWrapper=document.createElement('div');
+        imageWrapper.classList.add("search-result")
+
+        const image=document.createElement('img')
+        image.src=result.urls.small
+
+        const imageLink=document.createElement('a') ;
+        imageLink.href=result.links.html;
+        imageLink.target="_blank"
+        imageLink.textContent=result.alt_description
+
+
+        imageWrapper.append(image)
+        imageWrapper.append(imageLink)
+        searchRes.append(imageWrapper)
+    })
+    page++;
+    if(page>1){
+        showMore.style.display="block";
+    }
+}
+
+formEl.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    page=1;
+    searchImages();
+})
+showMore.addEventListener("click",(event)=>{
+    searchImages();
+})
